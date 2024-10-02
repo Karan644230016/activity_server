10d5f49ceb33cd6d83c29a142edf024994a23971
Karan644230016 <96411109+Karan644230016@users.noreply.github.com>
2024-10-02 14:15:34 +0700

    Update participate.js

--- controllers/participate.js
+++ controllers/participate.js
@@ -136,13 +136,18 @@ export const readManageOne = (req, res) => {
   });
 };
 export const updateStatus = (req, res) => {
-  const { par_status, std_ID, act_ID } = req.body;
+  let { par_status, std_ID, act_ID } = req.body;
+
+  // Ensure std_ID is an array
+  if (!Array.isArray(std_ID)) {
+    std_ID = [std_ID];
+  }
 
   const sql1 = `
-        UPDATE participate 
-        SET par_status = ?
-        WHERE act_ID = ? AND std_ID in (?)
-    `;
+    UPDATE participate 
+    SET par_status = ?
+    WHERE act_ID = ? AND std_ID IN (?)
+  `;
 
   db.query(sql1, [par_status, act_ID, std_ID], (err, result) => {
     if (err) {
@@ -157,6 +162,7 @@ export const updateStatus = (req, res) => {
     });
   });
 };
+
 
 
 
