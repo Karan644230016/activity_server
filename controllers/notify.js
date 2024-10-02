0237a235544d41c6bb818cf85093daca0589a1ae
Karan644230016 <96411109+Karan644230016@users.noreply.github.com>
2024-10-02 09:36:55 +0700

    Update notify.js

--- controllers/notify.js
+++ controllers/notify.js
@@ -16,7 +16,7 @@ export const get = (req, res) => {
 
 export const read = (req, res) => {
   const { news_ID, login_ID } = req.body;
-  const sql = `update notify set noti_status = "อ่านแล้ว" where news_ID = ? and login_ID = ?`;
+  const sql = `update notify set noti_status = "อ่านแล้ว" where noti_ID = ? and login_ID = ?`;
   db.query(sql, [news_ID, login_ID], (err, result) => {
     if (err) return res.status(500).json(err);
     return res.json(result);
